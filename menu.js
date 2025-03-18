a02956dfe8f6a0dc66dd4b9c597800b2f6422006
Leonardo <tu-correo@example.com>
2025-03-17 21:41:21 -0500

    Responsive principal

--- menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let menuToggle = document.getElementById("menu-toggle");
-    let navMenu = document.getElementById("nav-menu");
-
-    menuToggle.addEventListener("click", function() {
-        navMenu.classList.toggle("active");
-    });
-});
